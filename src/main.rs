c6691b7f47a767005cd5097573e20870e7458f3c
Joshua Enokson <enoksonprojects@protonmail.com>
2022-09-08 19:08:52 +0500

    removed dependencies, add verbose option, \ major refactoring, reduced mutex lockups, \ removed crossterm in favor of simple stout

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,48 @@
 use clap::{App, Arg};
-use std::{path::PathBuf, str::FromStr};
+use std::path::Path;
 
+// #[tokio::main]
 fn main() {
     let matches = App::new("cp-rs")
         .version("1.0")
         .author("Joshua Enokson <kilograhm@pm.me>")
-        .about("Copies files in concurency")
+        .about("Copies files in concurrency")
         .arg(
             Arg::new("src")
                 .multiple(true)
-//                .about("Sets the src dir to use")
                 .required(true)
-                .index(1),
+                // .index(1),
         )
         .arg(
             Arg::new("dest")
-//                .about("Sets the dest dir to use")
                 .required(true)
-                .index(2),
+                // .index(2),
+        )
+        .arg(
+            Arg::new("verbose")
+                .short('v')
+                // .index(2),
         )
         .get_matches();
 
-    let sources: Vec<PathBuf> = matches
+    let sources: Vec<&Path> = matches
         .values_of("src")
         .unwrap()
         .into_iter()
-        .map(|value| PathBuf::from_str(value).unwrap())
+        .map(|value| Path::new(value))
         .collect();
 
-    let dest = PathBuf::from_str(matches.value_of("dest").unwrap()).unwrap();
+    let dest = Path::new(matches.value_of("dest").unwrap());
+    let use_verbose = matches.is_present("verbose");
 
-    lib::main(sources, dest);
+    lib::main(&sources, &dest, use_verbose);
 }
 
 mod lib {
-    use crossterm::{cursor, execute, style, terminal};
     use num_cpus;
-    use std::collections::HashMap;
     use std::fs;
-    use std::io::{stdout, Stdout, Write};
-    use std::path::PathBuf;
+    use std::io::{stdout, Write};
+    use std::path::{Path, PathBuf};
     use std::process;
     use std::sync::{Arc, Mutex};
     use std::thread;
@@ -47,51 +50,21 @@ mod lib {
     use log::{error, LevelFilter};
     use syslog::{BasicLogger, Facility, Formatter3164};
 
-    #[derive(Debug, PartialEq, Eq)]
-    pub enum Task {
-        Initalizing,
-        Idle,
-        Scanning(PathBuf),
-        Coping(PathBuf),
-    }
-    impl Clone for Task {
-        fn clone(&self) -> Task {
-            match self {
-                Task::Idle => Task::Idle,
-                Task::Initalizing => Task::Initalizing,
-                Task::Coping(file) => Task::Coping(file.to_path_buf()),
-                Task::Scanning(dir) => Task::Scanning(dir.to_path_buf()),
-            }
-        }
-    }
-
-    #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+    #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
     pub enum Entry {
         File(PathBuf, PathBuf), // source, dest
         Dir(PathBuf, PathBuf),  // source, dest
     }
 
-    pub struct Worker {
-        task: Task,
-    }
-
     pub struct State {
-        pub sources: Mutex<Vec<PathBuf>>,
-        pub dest: Mutex<PathBuf>,
         pub entries: Mutex<Vec<Entry>>,
-        pub next_id: Mutex<u16>,
-        pub workers: Mutex<HashMap<u16, Worker>>,
-        pub stdout: Mutex<Stdout>,
-        // pub logger: Mutex<Logger<LoggerBackend, Formatter3164>>,
-        // pub stderror: Mutex<fs::File>,
-        pub entries_processed: Mutex<u64>,
     }
 
-    pub fn send_to_error(_state: Arc<State>, msg: String) {
+    pub fn send_to_error(msg: String) {
         error!("{}", msg);
     }
 
-    pub fn read_dir(src: &PathBuf, dir: &PathBuf, state: Arc<State>) -> Vec<Entry> {
+    pub fn read_dir<'a>(src: PathBuf, dir: &'a Path) -> Vec<Entry> {
         let mut entries = vec![];
         match fs::read_dir(dir) {
             Ok(read_dir) => {
@@ -100,101 +73,103 @@ mod lib {
                         Ok(entry) => match entry.file_type() {
                             Ok(file_type) => {
                                 if file_type.is_dir() {
-                                    entries.push(Entry::Dir(src.to_path_buf(), entry.path()))
+                                    entries.push(Entry::Dir(src.to_path_buf(), entry.path().to_path_buf()))
                                 } else if file_type.is_file() {
                                     entries.push(Entry::File(src.to_path_buf(), entry.path()))
                                 }
                             }
-                            Err(error) => send_to_error(state.clone(), error.to_string()),
+                            Err(error) => send_to_error(error.to_string()),
                         },
-                        Err(error) => send_to_error(state.clone(), error.to_string()),
+                        Err(error) => send_to_error(error.to_string()),
                     }
                 }
             }
-            Err(error) => send_to_error(state.clone(), error.to_string()),
+            Err(error) => send_to_error(error.to_string()),
         }
         entries.reverse();
         return entries;
     }
 
-    fn get_dest(src: &PathBuf, dest: &PathBuf, file: &PathBuf) -> PathBuf {
+    fn get_dest<'a>(src: &'a Path, dest: &'a Path, file: &'a Path) -> PathBuf {
         let relative = file.strip_prefix(src).expect("Not a prefix");
         dest.join(relative)
     }
 
-    pub fn mk_dir(src: &PathBuf, dest: &PathBuf, dir: &PathBuf, state: Arc<State>) {
+    pub fn mk_dir<'a>(src: &'a Path, dest: &'a Path, dir: &'a Path) {
         let new_dest = get_dest(src, dest, dir);
         if let Err(error) = fs::create_dir_all(new_dest) {
-            send_to_error(state.clone(), error.to_string())
+            send_to_error(error.to_string())
         }
     }
 
-    pub fn cp_file(src: &PathBuf, dest: &PathBuf, file: &PathBuf, state: Arc<State>) {
+    pub fn cp_file<'a>(src: &'a Path, dest: &'a Path, file: &'a Path) {
         let new_dest = get_dest(src, dest, file);
         if let Err(error) = fs::copy(file, new_dest) {
-            send_to_error(state.clone(), error.to_string())
+            send_to_error(error.to_string())
         }
     }
 
-    pub fn update_task(id: &u16, task: Task, padding: &u16, state: Arc<State>) {
-        let text: String;
-        match &task {
-            Task::Coping(file) => {
-                text = format!("Copying {}", file.display());
+    pub fn run_task<'a>(dest: PathBuf, state: Arc<State>, use_verbose: bool) -> thread::JoinHandle<()>{
+        thread::spawn(move || {
+            if use_verbose {
+                loop {
+                    let entry_options = { state.entries.lock().unwrap().pop() };
+                    match entry_options {
+                        Some(entry) => match entry {
+                            Entry::Dir(src, dir) => {
+                                {
+                                    let mut stdout = stdout().lock();
+                                    write!(&mut stdout, "{} => {}\n", src.to_string_lossy(), get_dest(&src, &*dest, &dir).to_str().unwrap()).unwrap();
+                                }
+                                mk_dir(&src, &dest, &dir);
+                                let mut new_entries = read_dir(src, &dir);
+                                {
+                                    let mut entries = state.entries.lock().unwrap();
+                                    entries.append(&mut new_entries);
+                                }
+                            }
+                            Entry::File(src, file) => {
+                                {
+                                    let mut stdout = stdout().lock();
+                                    write!(&mut stdout, "{} => {}\n", src.to_string_lossy(), get_dest(&src, &*dest, &file).to_str().unwrap()).unwrap();
+                                }
+                                cp_file(&src, &dest, &file);
+                            }
+                        },
+                        None => { break; }
+                    }
+                }
+            } else {
+                loop {
+                    let entry_options = { state.entries.lock().unwrap().pop() };
+                    match entry_options {
+                        Some(entry) => match entry {
+                            Entry::Dir(src, dir) => {
+                                mk_dir(&src, &dest, &dir);
+                                let mut new_entries = read_dir(src, &dir);
+                                {
+                                    let mut entries = state.entries.lock().unwrap();
+                                    entries.append(&mut new_entries);
+                                }
+                            }
+                            Entry::File(src, file) => {
+                                cp_file(&src, &dest, &file);
+                            }
+                        },
+                        None => { break; }
+                    }
+                }
             }
-            Task::Idle => {
-                text = format!("Idle");
-            }
-            Task::Initalizing => {
-                text = format!("Initializing");
-            }
-            Task::Scanning(dir) => {
-                text = format!("Scanning {}", dir.display());
-            }
-        }
-        let last_task = {
-            let mut workers = state.workers.lock().unwrap();
-            let mut worker = workers.get_mut(&id).unwrap();
-            let last_task = worker.task.clone();
-            worker.task = task.clone();
-            last_task
-        };
-        if last_task != task {
-            let mut stdout = state.stdout.lock().unwrap();
-            execute!(
-                stdout,
-                cursor::MoveTo(0, id + padding),
-                terminal::Clear(terminal::ClearType::UntilNewLine),
-                style::Print(text)
-            )
-            .unwrap();
-            stdout.flush().unwrap();
-        }
+            
+        })
     }
 
-    pub fn update_totals(state: Arc<State>) {
-        let entries_processed = { state.entries_processed.lock().unwrap() };
-        let entry_count = { state.entries.lock().unwrap().len() as u64 };
-        let mut stdout = state.stdout.lock().unwrap();
-        execute!(
-            stdout,
-            cursor::MoveTo(0, 0),
-            terminal::Clear(terminal::ClearType::UntilNewLine),
-            style::Print(format!("Entries processed: {}", entries_processed)),
-            cursor::MoveTo(0, 1),
-            terminal::Clear(terminal::ClearType::UntilNewLine),
-            style::Print(format!("Entries remaining: {}", entry_count))
-        )
-        .unwrap();
-        stdout.flush().unwrap();
-    }
-
-    pub fn main(sources: Vec<PathBuf>, dest: PathBuf) {
+    pub fn main<'a>(sources: &'a [ &'a Path ], dest: &'a Path, use_verbose: bool) {
         let formatter = Formatter3164 {
             facility: Facility::LOG_USER,
             hostname: None,
             process: "cp-rs".into(),
-            pid: process::id() as i32,
+            pid: process::id(),
         };
 
         let logger = syslog::unix(formatter).expect("could not connect to syslog");
@@ -204,13 +179,13 @@ mod lib {
 
         if sources.len() > 1 {
             if dest.is_file() {
-                panic!("If there are multiple sources, the desination must be a directory.");
+                panic!("If there are multiple sources, the destination must be a directory.");
             }
         }
 
         let mut entries: Vec<Entry> = vec![];
 
-        for entry in &sources {
+        for entry in sources {
             if entry.is_dir() {
                 let path_str = entry.to_str().expect("Could not get path_str");
                 if path_str.ends_with("/") {
@@ -225,126 +200,17 @@ mod lib {
             }
         }
 
-        let main_state = Arc::new(State {
-            sources: Mutex::new(sources),
-            dest: Mutex::new(dest.to_path_buf()),
-            entries: Mutex::new(entries),
-            next_id: Mutex::new(0),
-            workers: Mutex::new(HashMap::new()),
-            stdout: Mutex::new(stdout()),
-            entries_processed: Mutex::new(0),
+        let main_state: Arc<State> = Arc::new(State {
+            entries: Mutex::new(entries)
         });
 
-        const PADDING: u16 = 3;
         let cpu_count = num_cpus::get() as u64;
-
-        let entry_count = { main_state.entries.lock().unwrap().len() as u64 };
-        {
-            let mut stdout = main_state.stdout.lock().unwrap();
-            execute!(
-                stdout,
-                terminal::EnterAlternateScreen,
-                terminal::Clear(terminal::ClearType::All),
-                cursor::MoveTo(0, 0),
-                style::Print(format!("Entries remaining: {}", entry_count)),
-                cursor::MoveTo(0, 1),
-                style::Print(format!("Entries processed: {}", 0)),
-                cursor::MoveTo(0, 2),
-                style::Print(format!("Threads: {}", cpu_count))
-            )
-            .unwrap();
-            stdout.flush().unwrap();
+        let mut handles = vec![];
+        for _ in 0..cpu_count {
+            handles.push(run_task(dest.to_path_buf(), main_state.clone(), use_verbose));
         }
-
-        let handles = (0..cpu_count)
-            .into_iter()
-            .map(|_| {
-                let state = main_state.clone();
-                let id = {
-                    let mut next_id = state.next_id.lock().unwrap();
-                    let id = *next_id;
-                    *next_id += 1;
-                    id
-                };
-                let worker = Worker {
-                    task: Task::Initalizing,
-                };
-                {
-                    let mut workers = state.workers.lock().unwrap();
-                    workers.insert(id, worker);
-                }
-                update_task(&id, Task::Initalizing, &PADDING, state.clone());
-                let dest = { state.dest.lock().unwrap().to_path_buf() };
-                thread::spawn(move || {
-                    loop {
-                        let entry_options = { state.entries.lock().unwrap().pop() };
-                        match entry_options {
-                            Some(entry) => match entry {
-                                Entry::Dir(src, dir) => {
-                                    update_task(
-                                        &id,
-                                        Task::Scanning(dir.to_path_buf()),
-                                        &PADDING,
-                                        state.clone(),
-                                    );
-                                    // thread::sleep(Duration::from_secs(2));
-                                    mk_dir(&src, &dest, &dir, state.clone());
-                                    let mut new_entries = read_dir(&src, &dir, state.clone());
-                                    {
-                                        let mut entries = state.entries.lock().unwrap();
-                                        entries.append(&mut new_entries);
-                                    }
-                                    {
-                                        let mut dirs_processed =
-                                            state.entries_processed.lock().unwrap();
-                                        *dirs_processed += 1;
-                                    };
-                                    update_totals(state.clone())
-                                }
-                                Entry::File(src, file) => {
-                                    update_task(
-                                        &id,
-                                        Task::Coping(file.to_path_buf()),
-                                        &PADDING,
-                                        state.clone(),
-                                    );
-                                    // thread::sleep(Duration::from_secs(2));
-                                    cp_file(&src, &dest, &file, state.clone());
-                                    {
-                                        let mut files_processed =
-                                            state.entries_processed.lock().unwrap();
-                                        *files_processed += 1;
-                                    };
-                                    update_totals(state.clone())
-                                }
-                            },
-                            None => {}
-                        }
-                        update_task(&id, Task::Idle, &PADDING, state.clone());
-                        let workers = state.workers.lock().unwrap();
-                        let mut should_break = true;
-                        for (_id, worker) in workers.iter() {
-                            match worker.task {
-                                Task::Idle => {}
-                                _ => should_break = false,
-                            }
-                        }
-                        if should_break {
-                            break;
-                        }
-                    }
-                })
-            })
-            .collect::<Vec<thread::JoinHandle<_>>>();
-
         for thread in handles {
             thread.join().unwrap();
-        }
-
-        {
-            let mut stdout = main_state.stdout.lock().unwrap();
-            execute!(stdout, terminal::LeaveAlternateScreen).unwrap();
-            stdout.flush().unwrap();
         }
     }
 }
